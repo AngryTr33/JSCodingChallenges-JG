68e8dbeb23bb4c417434ba7b8dc966a6d752daac
Joe Gastring <Joe_Gastring@yahoo.com>
2022-07-10 22:19:46 -0500

    Challenge 6 complete

--- Challenge06/site06.js
+++ Challenge06/site06.js
@@ -18,14 +18,40 @@ function findVowels() {
         alert("Error: Vowel Object is Empty");
         return;
     }
-
+    //original
+    //document.getElementById("results").innerHTML = `You entered <strong>"${str}"</strong> it has ${vowelObj} vowels`;
+    //ex credit
     document.getElementById("results").innerHTML = `You entered <strong>"${str}"</strong> it has ${vowelObj.vCount} vowels.<br>The vowels found are ${vowelObj.vFound.toString()}`;
 
 }
 
 //takes an string and returns the vowel count and the vowels found as an object.
 function getVowelCount(str) {
-    
-    return {};
+    //initialize variable
+    let vowelCount = 0;
+    let vArray = ['a','e','i','o','u'];
+    //ex credit
+    let foundVowels = [];
+    //convert string to lowercase
+    str = str.toLowerCase();
+    //loop over each char
+    for (let i = 0; i < str.length; i++) {
+        //check to see if char exists in array 
+        if (vArray.includes(str[i])){
+            //increment the vowel count
+            vowelCount ++;
+            //ex credit
+            foundVowels.push(str[i]);
+        }        
+    }
+    //ex credit
+    let vowelObj = {
+        vCount : vowelCount,
+        vFound : foundVowels
+    }
+    //ex credit
+    return vowelObj;
+    //return the vowel count
+    //return vowelCount;
 
 }
